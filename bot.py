d2c1da7bebcb832aa7442ec3cd33849b7880b0ed
Nicolás Gebauer <80332183+nicolasgebauer@users.noreply.github.com>
2022-11-04 18:27:28 -0300

    idudh

--- bot.py
+++ bot.py
@@ -4,8 +4,9 @@ bot = telebot.TeleBot("5670463206:AAEoQE14qn2_TqV0qmyyRK5kgDv-BJmDxto")
 @bot.message_handler(commands=["help","start"])
 
 
-def enviar(message):
+def numeros(message):
     bot.reply_to(message, "Hola,como estas?")
-
+    for i in range(10):
+        bot.reply_to(message, i)
 
 bot.polling()
