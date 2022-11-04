2008194fbb3598f91746b4ea20b2bc85147f88dc
Nicolás Gebauer <80332183+nicolasgebauer@users.noreply.github.com>
2022-11-04 20:27:09 -0300

    lll

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ import requests
 from flask import Flask, jsonify, request
 import json
 
-TOKEN = "5670463206:AAEoQE14qn2_TqV0qmyyRK5kgDv-BJmDxto"
+TOKEN = '5670463206:AAEoQE14qn2_TqV0qmyyRK5kgDv-BJmDxto'
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET","POST"])
+@app.route("/", methods = ['GET','POST'])
 
 def hello_word():
-    if request.method == "POST":
+    if request.method == 'POST':
         data = request.get_json()
-        print(f"DATA: {data}")
+        print(f'DATA: {data}')
         return {'satatusCode':200, 'body':'Success', 'data':data}
     else:
         return {'satatusCode':200, 'body':'Success'}
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
