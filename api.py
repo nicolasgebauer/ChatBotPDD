036cccb288f718dadcbab4dd6627db3c05b0850e
Jorge Plaza de los Reyes <80331241+jorgeplazadelosreyes@users.noreply.github.com>
2022-11-05 18:09:57 -0300

    create gn debug

--- api.py
+++ api.py
@@ -8,8 +8,8 @@ def create_user(tel_id, username, lobby_id):
     data = {"username": username, "telegram_id": tel_id, "lobby": lobby_id, "won_number": 0, "won_trivia": 0, "won_third": 0, "number_tries": 0}
     data_check = {"tel_id": tel_id, "lobby_id": lobby_id}
     users = requests.get(f'{api_url}user_created/', data_check)
-    print("Usuarios:", list(users.content))
-    if len(list(users.content)) > 0:
+    print("Usuarios:", list(users))
+    if len(list(users)) > 0:
         return 2 ## ya existe
     response = requests.post(f'{api_url}users/', json=data)
     print("CREACION DE USUARIO:", response.content)
