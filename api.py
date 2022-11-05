d28a193a94146e146075dc898597888c67265217
Jorge Plaza de los Reyes <80331241+jorgeplazadelosreyes@users.noreply.github.com>
2022-11-05 16:55:03 -0300

    create user debug json

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ api_url = 'https://apipds4.herokuapp.com/'
 def create_user(tel_id, username, lobby_id):
     data = {"username": username, "telegram_id": tel_id, "lobby": lobby_id, "won_number": 0, "won_trivia": 0, "won_third": 0, "number_tries": 0}
     print(data)
-    response = requests.post(f'{api_url}users/', data)
+    response = requests.post(f'{api_url}users/', json=data)
     print(response.content)
     if response.status_code == 200:
         print(response.content)
