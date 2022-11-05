79798e314411f8f6801aef8e262ffa43ee234c90
Jorge Plaza de los Reyes <80331241+jorgeplazadelosreyes@users.noreply.github.com>
2022-11-05 17:18:45 -0300

    check if exist test response check

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ def create_user(tel_id, username, lobby_id):
     data = {"username": username, "telegram_id": tel_id, "lobby": lobby_id, "won_number": 0, "won_trivia": 0, "won_third": 0, "number_tries": 0}
     data_check = {"tel_id": tel_id, "lobby_id": lobby_id}
     usuarios = requests.get(f'{api_url}user_created/', json=data_check)
-    print("Usuarios:", usuarios)
+    print("Usuarios:", usuarios.content)
     if len(usuarios) > 0:
         return 2 ## ya existe
     #response = requests.post(f'{api_url}users/', data)
