69af0738ba51f1e5d405b1ad2f7dceb480357bf0
Jorge Plaza de los Reyes <80331241+jorgeplazadelosreyes@users.noreply.github.com>
2022-11-05 16:33:29 -0300

    create user

---
+++ api.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+api_url = 'https://apipds4.herokuapp.com/'
+
+def create_user(tel_id, username, lobby_id):
+    data = {"telegram_id": tel_id, "username": username, "lobby": lobby_id}
+    response = requests.post(f'{api_url}users', json=data)
+    if response.status_code == 200:
+        print(response.content)
+        return True
+    return False
--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import requests
 from flask import Flask, jsonify, request
 import json
+import api
 
 TOKEN = '5670463206:AAEoQE14qn2_TqV0qmyyRK5kgDv-BJmDxto'
+api_url = 'https://apipds4.herokuapp.com/'
 # https://api.telegram.org/bot5670463206:AAEoQE14qn2_TqV0qmyyRK5kgDv-BJmDxto/setWebhook?url=https://pdd-games.herokuapp.com
 app = Flask(__name__)
 
@@ -20,14 +22,26 @@ def welcome_message(item):
         to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={welcome_msg}&parse_mode=HTML'
         resp = requests.get(to_url)
 
+def create_user(item):
+    if item["text"].lower() == "create user":
+        user_id = item["from"]["id"]
+        username = item["from"]["first_name"]
+        chat_id = item["chat"]["id"]
+        msg = ""
+        if api.create_user(user_id, username, chat_id):     
+            msg = f"Usuario {username} creado con exito, bienvenid@!"
+        else:
+            msg = f"Error al crear usuario"
+        to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML'
+        resp = requests.get(to_url)
 
 def set_numbers(item):
     print(item)
     sets = item["text"].split()
+    chat_id = item["chat"]["id"]
     try:
         if sets[0].lower() == "numbers" and int(sets[1]) > 0 and int(sets[2]) > 0:
             msg = f"Welcome to de game Numbers >>> max = {sets[1]} intentos = {sets[2]}"
-            chat_id = item["chat"]["id"]
             user_id = item["from"]["id"]
             username = item["from"]["first_name"]
             final_msg = f'{msg}'
@@ -35,7 +49,6 @@ def set_numbers(item):
             to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={final_msg}&parse_mode=HTML'
             resp = requests.get(to_url)
     except:
-        chat_id = item["chat"]["id"]
         final_msg = f'Sintax error to set numbers'
         to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={final_msg}&parse_mode=HTML'
         resp = requests.get(to_url)
@@ -49,6 +62,7 @@ def hello_word():
             data = data["message"]
             welcome_message(data)
             set_numbers(data)
+            create_user(data)
             return {"statusCode": 200, "body": "Success", "data": data}
         else:        
             return {'satatusCode':404, 'body':'User has left the chatroom and deleted the chat', 'data':data}
