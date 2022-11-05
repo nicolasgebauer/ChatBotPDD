a16d83103f4b3f9ce0a974dc81d42f8f257a3fbf
Jorge Plaza de los Reyes <80331241+jorgeplazadelosreyes@users.noreply.github.com>
2022-11-04 21:55:42 -0300

    hello msg

--- app.py
+++ app.py
@@ -6,13 +6,32 @@ TOKEN = '5670463206:AAEoQE14qn2_TqV0qmyyRK5kgDv-BJmDxto'
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['GET','POST'])
 
+def welcome_message(item):
+    print(item)
+
+    if item["text"].lower() == "hi":
+        msg = "hello"
+        chat_id = item['chat']['id']
+        user_id = item["from"]["id"]
+        username = item['from']["username"]
+        welcome_msg = f'{msg} {username}'
+
+        to_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={welcome_msg}&parse_mode=HTML"
+        resp = requests.get(to_url)
+
+
+@app.route("/", methods = ['GET','POST'])
 def hello_word():
     if request.method == 'POST':
         data = request.get_json()
         print(f'DATA: {data}')
-        return {'satatusCode':200, 'body':'Success', 'data':data}
+        if "message" in data:
+            data = data["message"]
+            welcome_message(data)
+            return {"statusCode": 200, "body": "Success", "data": data}
+        else:        
+            return {'satatusCode':404, 'body':'User has left the chatroom and deleted the chat', 'data':data}
     else:
         return {'satatusCode':200, 'body':'Success'}
 
