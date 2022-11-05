264bc305192899365c80d94687001e8e8803842a
Nicolás Gebauer <80332183+nicolasgebauer@users.noreply.github.com>
2022-11-05 10:49:40 -0300

    lol

--- app.py
+++ app.py
@@ -20,6 +20,18 @@ def welcome_message(item):
         to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={welcome_msg}&parse_mode=HTML'
         resp = requests.get(to_url)
 
+def set_numbers(item):
+    print(item)
+
+    if item["text"].lower() == "numbers":
+        msg = "Welcomme tu de game Numbers"
+        chat_id = item["chat"]["id"]
+        user_id = item["from"]["id"]
+        username = item["from"]["first_name"]
+        final_msg = f'{msg}'
+
+        to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={final_msg}&parse_mode=HTML'
+        resp = requests.get(to_url)
 
 @app.route("/", methods = ['GET','POST'])
 def hello_word():
