127972772b4e33355760b2d36ed0897d1ddc41c2
Jorge Plaza de los Reyes <80331241+jorgeplazadelosreyes@users.noreply.github.com>
2022-11-05 16:50:45 -0300

    create user debug

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ def create_user(tel_id, username, lobby_id):
     data = {"username": username, "telegram_id": tel_id, "lobby": lobby_id, "won_number": 0, "won_trivia": 0, "won_third": 0, "number_tries": 0}
     print(data)
     response = requests.post(f'{api_url}users/', data)
-    print(response)
+    print(response.content)
     if response.status_code == 200:
         print(response.content)
         return True
