f55ef2737bba8119bf946fe2c97007f41fa37489
Jorge Plaza de los Reyes <80331241+jorgeplazadelosreyes@users.noreply.github.com>
2022-11-06 19:45:25 -0300

    chat id str

--- app.py
+++ app.py
@@ -23,9 +23,10 @@ def create_user(item):
     if item["text"].lower() == "create user":
         user_id = str(item["from"]["id"])
         username = str(item["from"]["first_name"])
-        chat_id = str(item["chat"]["id"])
+        chat_id = item["chat"]["id"]
+        chat_id_str = str(chat_id)
         msg = ""
-        user = api.create_user(user_id, username, chat_id)
+        user = api.create_user(user_id, username, chat_id_str)
         if  user == 1:     
             msg = f"Usuario {username} creado con exito, bienvenid@!"
         elif user == 2:
@@ -37,16 +38,18 @@ def create_user(item):
 
 def is_game_numbers_active(item):
     chat_id = item["chat"]["id"]
-    if api.get_game_numbers_activate(chat_id):
+    chat_id_str = str(chat_id)
+    if api.get_game_numbers_activate(chat_id_str):
         set_guess(item)
 
 
 def set_numbers(item):
     sets = item["text"].split()
     chat_id = item["chat"]["id"]
+    chat_id_str = str(chat_id)
     try:
         if sets[0].lower() == "numbers" and int(sets[1]) > 0 and int(sets[2]) > 0:
-            game = api.create_number_game(chat_id, int(sets[1]), int(sets[2]))
+            game = api.create_number_game(chat_id_str, int(sets[1]), int(sets[2]))
             msg = ""
             if game == 1:
                 msg = f"Juego Numbers iniciado >>> max = {sets[1]} intentos = {sets[2]}."
@@ -65,14 +68,15 @@ def set_numbers(item):
 
 def set_guess(item):
     sets = str(item["text"])
-    chat_id = str(item["chat"]["id"])
+    chat_id = item["chat"]["id"]
+    chat_id_str = str(chat_id)
     user_id = str(item["from"]["id"])
     username = item["from"]["first_name"]
     error = False
     msg_error = ""
     try:
-        if sets.isnumeric() and api.user_numbers_tries(chat_id,user_id) > 0:
-            game = api.guess_number(chat_id, int(sets))
+        if sets.isnumeric() and api.user_numbers_tries(chat_id_str,user_id) > 0:
+            game = api.guess_number(chat_id_str, int(sets))
             msg = ""
             if game == 1:
                 if api.won_number_games(chat_id, user_id):
@@ -89,16 +93,16 @@ def set_guess(item):
                     msg_error = "Error al subir los juegos numbers ganados."
                     error = True
             elif game == 2:
-                if api.tries_down(chat_id,user_id):
-                    tries = api.user_numbers_tries(chat_id,user_id)
+                if api.tries_down(chat_id_str,user_id):
+                    tries = api.user_numbers_tries(chat_id_str,user_id)
                     msg_upper_number_guess = f"El numero es mayor a {sets}."
                     to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg_upper_number_guess}&parse_mode=HTML'
                     resp = requests.get(to_url)
                     msg_rest_tries = f"{username} te quedan {tries} intentos."
                     to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg_rest_tries}&parse_mode=HTML'
                     resp = requests.get(to_url)
-                    if not api.check_total_tries(chat_id):
-                        if api.end_game_numbers(chat_id):
+                    if not api.check_total_tries(chat_id_str):
+                        if api.end_game_numbers(chat_id_str):
                             msg_game_losers = f"Juego terminado, ya no quedan más intentos."
                             to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg_game_losers}&parse_mode=HTML'
                             resp = requests.get(to_url)
@@ -109,16 +113,16 @@ def set_guess(item):
                     msg_error = "Error al bajar los intentos"
                     error = True
             elif game == 3:
-                if api.tries_down(chat_id,user_id):
-                    tries = api.user_numbers_tries(chat_id,user_id)
+                if api.tries_down(chat_id_str,user_id):
+                    tries = api.user_numbers_tries(chat_id_str,user_id)
                     msg_lower_number_guess = f"El numero es menor a {sets}."
                     to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg_lower_number_guess}&parse_mode=HTML'
                     resp = requests.get(to_url)
                     msg_rest_tries = f"{username} te quedan {tries} intentos."
                     to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg_rest_tries}&parse_mode=HTML'
                     resp = requests.get(to_url)                                        
-                    if not api.check_total_tries(chat_id):
-                        if api.end_game_numbers(chat_id):
+                    if not api.check_total_tries(chat_id_str):
+                        if api.end_game_numbers(chat_id_str):
                             msg_game_losers = f"Juego terminado, ya no quedan más intentos."
                             to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg_game_losers}&parse_mode=HTML'
                             resp = requests.get(to_url)
