f98a8a15eb2ca44c4b34eb6721bd5f0cdb2a2eb6
Jorge Plaza de los Reyes <80331241+jorgeplazadelosreyes@users.noreply.github.com>
2022-11-29 08:08:00 -0300

    set guess trivia: new question options message

--- app.py
+++ app.py
@@ -237,12 +237,10 @@ def set_guess_trivia_first(item):
                 msg_question = f"Pregunta {next_q+1}: {question}"
                 send_msg(chat_id, msg_question)
                 keyboard = setKeyboard(options)
+                msg_question = ""
+                for i in range(len(options)):
+                    msg += f"{chr(i+97)}) {options[i]}\n"
                 sendTextWithButtons(chat_id, msg_question, keyboard)
-                #send_msg(chat_id, msg_question)
-                #msg = ""
-                #for i in range(len(options)):
-                #    msg += f"{chr(i+97)}) {options[i]}\n"
-                #send_msg(chat_id_str,msg)
         elif game ==2:
             msg = f"Respuesta {sets} es correcta, {username}"
             send_msg(chat_id,msg)
