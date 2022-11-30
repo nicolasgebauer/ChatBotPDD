d14b8f37b554c648360486d54c8b3a0ae7da1997
Nicolás Gebauer <80332183+nicolasgebauer@users.noreply.github.com>
2022-11-29 22:49:21 -0300

    try to fix +

--- math_game.py
+++ math_game.py
@@ -20,9 +20,12 @@ def create_math_games_params():
 
     str_ope = ""
     str_ope += str(numbers_in[0])
+    str_ope += " "
     for i in range(3):
         str_ope += str(operations_in[i])
+        str_ope += " "
         str_ope += str(numbers_in[i+1])
+        str_ope += " "
 
 
     while True:
@@ -44,7 +47,7 @@ def create_math_games_params():
             numbers_in.pop(1)
         else:
             break
-    return(numbers_in[0],str_ope)        
+    return(numbers_in[0],str_ope[:-1])        
 
 def is_negative_number_digit(n: str) -> bool:
      try:
